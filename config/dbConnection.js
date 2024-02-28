c9606a1c48f38bff63d2995eb2fa0d934f153627
hemarawal <56906330+hemarawal@users.noreply.github.com>
2024-02-28 22:27:48 +0530

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    // Log the value of MONGO_URL
+    console.log('MONGO_URL:', process.env.MONGO_URL);
+
     // Connect with the database...
     const connect = await mongoose.connect(process.env.MONGO_URL);
     console.log(

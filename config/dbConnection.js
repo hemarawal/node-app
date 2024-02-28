f19df1f1c2a74f0207f2e753f978d80a7bdc5c70
hemarawal <56906330+hemarawal@users.noreply.github.com>
2024-02-28 22:48:54 +0530

    Update again dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // Log the value of MONGO_URL
-    console.log('MONGO_URL:', process.env.MONGO_URL);
-
     // Connect with the database...
-    const connect = await mongoose.connect(process.env.MONGO_URL);
+    const connect = await mongoose.connect('mongodb://<host_ip>:27017/mydatabase');
     console.log(
       "Database connected successfully...",
       connect.connection.host,
